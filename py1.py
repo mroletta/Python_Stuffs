b1a0f80b63651b124d7421e60aa774cc1c3f34f5
mroletta <mroletta@gmail.com>
2013-10-17 11:37:10 -0500

    Continued tutorial

--- py1.py
+++ py1.py
@@ -1,56 +1,56 @@
 import wx
-import os
+class ExamplePanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.quote = wx.StaticText(self, label="Programatron 9000", pos=(20, 30))
 
-class MainWindow(wx.Frame):
-	def __init__(self, parent, title):
-		wx.Frame.__init__(self, parent, title=title, size=(200,100))
-		self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-		self.CreateStatusBar() # A status bar at the bottom of the MainWindow
+        # A multiline TextCtrl - This is here to show how the events work in this program, don't pay too much attention to it
+        self.logger = wx.TextCtrl(self, pos=(300,20), size=(200,300), style=wx.TE_MULTILINE | wx.TE_READONLY)
 
-		#setting up the menu
-		filemenu= wx.Menu()
+        # A button
+        self.button =wx.Button(self, label="Save", pos=(200, 325))
+        self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
 
-		#wx.ID_ABOUT and wx.IDE_EXIT are standard IDs provided by wxWidgets.
-		menuAbout = filemenu.Append(wx.ID_ABOUT, "&About","Information about this program")
-		menuExit = filemenu.Append(wx.ID_EXIT, "E&xit","Terminte the program")
-		menuOpen = filemenu.Append(wx.ID_OPEN, "&Open a File")
+        # the edit control - one line version.
+        self.lblname = wx.StaticText(self, label="Your name :", pos=(20,60))
+        self.editname = wx.TextCtrl(self, value="Enter here your name", pos=(150, 60), size=(140,-1))
+        self.Bind(wx.EVT_TEXT, self.EvtText, self.editname)
+        self.Bind(wx.EVT_CHAR, self.EvtChar, self.editname)
 
+        # the combobox Control
+        self.sampleList = ['friends', 'advertising', 'web search', 'Yellow Pages']
+        self.lblhear = wx.StaticText(self, label="How did you hear from us ?", pos=(20, 90))
+        self.edithear = wx.ComboBox(self, pos=(150, 90), size=(95, -1), choices=self.sampleList, style=wx.CB_DROPDOWN)
+        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.edithear)
+        self.Bind(wx.EVT_TEXT, self.EvtText,self.edithear)
 
-		#Create the menu bar
-		menuBar = wx.MenuBar()
-		menuBar.Append(filemenu, "&File")
-		self.SetMenuBar(menuBar)
-		
-		# Set events
-		self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-		self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-		self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        # Checkbox
+        self.insure = wx.CheckBox(self, label="Do you want Insured Shipment ?", pos=(20,180))
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.insure)
 
-		self.Show(True)
+        # Radio Boxes
+        radioList = ['blue', 'red', 'yellow', 'orange', 'green', 'purple', 'navy blue', 'black', 'gray']
+        rb = wx.RadioBox(self, label="What color would you like ?", pos=(20, 210), choices=radioList,  majorDimension=3,
+                         style=wx.RA_SPECIFY_COLS)
+        self.Bind(wx.EVT_RADIOBOX, self.EvtRadioBox, rb)
 
-	def OnAbout(self,e):
-		# A message dialog box with an OK button.  wx.OK is standard ID in wxWidgets
-		dlg = wx.MessageDialog(self, "A small text editor", "About sample editor", wx.OK)
-		dlg.ShowModal ()
-		dlg.Destroy
-
-	def OnExit(self,e):
-		self.Close(True) # Close the frame
-
-	def OnOpen(self,e):
-		"""Open a file"""
-		self.dirname = ''
-		dlg = wx.FileDialog(self, "Chose a file", self.dirname, "", "*.*", wx.OPEN)
-		if dlg.ShowModal() == wx.ID_OK:
-			self.filename = dlg.GetFilename()
-			self.dirname = dlg.GetDirectory()
-			f = open(os.path.join(self.dirname, self.filename), 'r')
-			self.control.SetValue(f.read())
-			f.close()
-		dlg.Destroy()	
-
+    def EvtRadioBox(self, event):
+        self.logger.AppendText('EvtRadioBox: %d\n' % event.GetInt())
+    def EvtComboBox(self, event):
+        self.logger.AppendText('EvtComboBox: %s\n' % event.GetString())
+    def OnClick(self,event):
+        self.logger.AppendText(" Click on object with Id %d\n" %event.GetId())
+    def EvtText(self, event):
+        self.logger.AppendText('EvtText: %s\n' % event.GetString())
+    def EvtChar(self, event):
+        self.logger.AppendText('EvtChar: %d\n' % event.GetKeyCode())
+        event.Skip()
+    def EvtCheckBox(self, event):
+        self.logger.AppendText('EvtCheckBox: %d\n' % event.Checked())
 
 
 app = wx.App(False)
-frame = MainWindow(None, "Sample editor")
+frame = wx.Frame(None)
+panel = ExamplePanel(frame)
+frame.Show()
 app.MainLoop()
